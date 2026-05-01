8d9e946b646bbc925052a71dbd06dd557994f128
Ayush Kumar Samal <ayushkumarsamal2709@gmail.com>
2026-05-02 01:21:45 +0530

    Implement voice assistant features in main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+from voice.listen import listen
+from voice.speak import speak
+from physics.qa import answer_physics
+import webbrowser
+import datetime
+import os
 
+def run():
+    speak("Jarvis Physics Assistant activated")
+
+    while True:
+        command = listen()
+
+        if "time" in command:
+            time = datetime.datetime.now().strftime("%H:%M")
+            speak(f"The time is {time}")
+
+        elif "open google" in command:
+            webbrowser.open("https://google.com")
+
+        elif "open code" in command:
+            os.system("code")
+
+        elif "physics" in command or "define" in command or "explain" in command:
+            response = answer_physics(command)
+            speak(response)
+
+        elif "stop" in command:
+            speak("Goodbye")
+            break
+
+run()
